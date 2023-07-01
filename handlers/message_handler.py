d598773b72455d73280fedeb5f675bbe1cc711b3
Demorey <demorey2@gmail.com>
2023-07-01 07:11:22 +0300

    Добавлена обработка команды старт

---
+++ handlers/message_handler.py
@@ -0,0 +1,8 @@
+from create_bot import dp, bot
+from aiogram import types
+
+
+@dp.message_handler(commands='start')
+async def command_start(message: types.Message):
+    print(message)
+    # await bot.send_message(, f'id группы: {message.chat.id} \n')
