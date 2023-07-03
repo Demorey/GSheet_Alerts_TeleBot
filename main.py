db75f21b3587541cf48c240769a3ecec604beb00
Demorey <demorey2@gmail.com>
2023-07-03 15:50:07 +0300

    Добавлен вызов обработчиков по таймеру

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import asyncio
+import json
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-from aiogram.utils import executor
-from create_bot import dp, bot
+import gspread
+
 import handlers.message_handler
+from aiogram.utils import executor
+
+import notification_sender
+from create_bot import dp, bot, TIMER
+from handlers.gsheets_handler import spreadsheet_check
 
 if not os.path.exists("logs/debug_loger.log"):
     os.makedirs("logs/")
@@ -17,12 +24,27 @@ logging.basicConfig(level=logging.DEBUG, encoding='utf-8',
 
 async def on_startup(_):
     bot_name = await bot.get_me()
-    print('Запущен бот: '+bot_name.first_name)
+    print('Запущен бот: ' + bot_name.first_name)
+
+
+async def task():
+    while True:
+        gc = gspread.service_account(filename='data/service_acc.json')
+
+        with open('data/spreadsheets_data.json', 'r', encoding='utf-8') as f:
+            spreadsheet_data = json.load(f)
+        spreadsheet_list = spreadsheet_data["SPREADSHEETS"]
+        for i, spreadsheet in enumerate(spreadsheet_list):
+            result = await spreadsheet_check(gc, i, spreadsheet, spreadsheet_data)
+            if result:
+                await notification_sender.send_notification(result, spreadsheet["url"])
+
+        await asyncio.sleep(TIMER)
 
 
 if __name__ == '__main__':
-    # loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-    # loop.create_task(handlers.client.task())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(task())
 
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
