da3021a6a8e715197ca3bc52031315c73da9b685
Demorey <demorey2@gmail.com>
2024-03-22 17:20:03 +0300

    add new reply_markup

--- notification_sender.py
+++ notification_sender.py
@@ -1,19 +1,20 @@
 import json
 
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from create_bot import bot
 
 
 async def send_notification(group_id, text, url):
-    reply_markup = InlineKeyboardMarkup(row_width=1)
-    reply_markup.add(InlineKeyboardButton(f'Перейти к таблице', url=url))
+    reply_markup = InlineKeyboardBuilder()
+    reply_markup.row(InlineKeyboardButton(text="Перейти к таблице", url=url))
     if text.startswith("Ошибка"):
         with open('data/config.json', 'r', encoding='utf-8') as f:
             config = json.load(f)
         group_id = config["ADMIN_ID"]
     if len(text) > 4096:
         for x in range(0, len(text), 4096):
-            await bot.send_message(group_id, text[x:x + 4096], parse_mode="HTML", reply_markup=reply_markup)
+            await bot.send_message(group_id, text[x:x + 4096], parse_mode="HTML", reply_markup=reply_markup.as_markup())
     else:
-        await bot.send_message(group_id, text, parse_mode="HTML", reply_markup=reply_markup)
+        await bot.send_message(group_id, text, parse_mode="HTML", reply_markup=reply_markup.as_markup())
